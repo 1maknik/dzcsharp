a9cb6abc1f72207eb92a74b1991c4d5eee88a361
nik <1maknik@mail.ru>
2022-07-14 01:36:38 +0300

    z001

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//С клавиатуры вводится целое число. Вывести квадрат числа
+double a;
+double b;
+string? s;
+s=Console.ReadLine();
+a=Convert.ToDouble(s);
+b=a*a;
+System.Console.WriteLine($"{a}^2={b}");
