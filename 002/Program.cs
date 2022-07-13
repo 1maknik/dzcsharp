75bd335b2ef7bca8168987cb2136d2296e5d17c3
nik <1maknik@mail.ru>
2022-07-14 02:17:25 +0300

    z002

---
+++ 002/Program.cs
@@ -0,0 +1,20 @@
+﻿//С клавиатуры вводятся два вещественных числа. Проверять является ли одно из них квадратом второго
+double a,b;
+string? s=Console.ReadLine();
+a=Convert.ToDouble(s);
+s=Console.ReadLine();
+b=Convert.ToDouble(s);
+if (a*a==b)
+{
+    System.Console.WriteLine("b квадрат a");
+}
+else{
+    System.Console.WriteLine("b не квадрат a");
+}
+if (a==b*b)
+{
+    System.Console.WriteLine("а квадрат b");
+}
+else{
+    System.Console.WriteLine("a не квадрат b");
+}
