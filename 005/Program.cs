0feac4b94a8b28f8531f0e62b9757883e35d175b
nik <1maknik@mail.ru>
2022-07-14 12:24:46 +0300

    z005

---
+++ 005/Program.cs
@@ -0,0 +1,17 @@
+﻿Console. Write ("a = ") ;
+
+double a = double. Parse (Console. ReadLine ()) ;
+
+Console. Write ("b = ") ;
+
+double b = double. Parse (Console. ReadLine ()) ;
+
+Console. Write ("c = ") ;
+
+double c = double. Parse (Console. ReadLine ()) ;
+
+if (a < b) a = b;
+
+if (a < c) a = c;
+
+Console. WriteLine ("Max = {0}", a) ;
