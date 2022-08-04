e18747bfce39a22b41d0671dd74accdccd02c8b0
nik <1maknik@mail.ru>
2022-08-04 23:18:14 +0300

    z006

---
+++ 006/Program.cs
@@ -0,0 +1,7 @@
+﻿double x=Math.PI;
+double y=Math.Sin(x);
+double y1=Math.Sin(x*2);
+System.Console.WriteLine($"{y:G}");
+System.Console.WriteLine(9.0/100000000.0);
+if (Math.Abs(y-y1)<=1E-10) System.Console.WriteLine("OK");
+else System.Console.WriteLine("Wrong");
