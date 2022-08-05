927e94aa3669b35da75ec9b9de5156fe4dddfc17
nik <1maknik@mail.ru>
2022-08-05 18:43:18 +0300

    z017

---
+++ 017/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine();
+
+int dayNumber = 5;
+string[] Days = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
+string txtResult = "";
+
+if (dayNumber > 5) txtResult = $". выходной";
+else txtResult = $", рабочий ";
+
+System.Console.WriteLine();
+System.Console.WriteLine($"{dayNumber}-й день недели - это {Days[dayNumber - 1]}{txtResult}");
+System.Console.WriteLine();
