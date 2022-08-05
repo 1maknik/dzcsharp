5ffa8aa4fe3ae98b3f732083fc57c64336c634ca
nik <1maknik@mail.ru>
2022-08-05 17:44:19 +0300

    z027

---
+++ 027/Program.cs
@@ -0,0 +1,10 @@
+﻿ Console.Write("Введите x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double S = 1;
+            Console.Write("Введите кол-во повторов: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                S *= x;
+            }
+            Console.WriteLine(S);
