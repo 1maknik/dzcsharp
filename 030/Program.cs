9cf178aaf1f944f03e31e113d087b5df6221f3c3
nik <1maknik@mail.ru>
2022-08-05 18:12:05 +0300

    z030

---
+++ 030/Program.cs
@@ -0,0 +1,13 @@
+﻿int a, b, p = 1;
+Console.WriteLine("Введите число a:");
+a = Convert.ToInt32(Console.ReadLine());
+ 
+Console.WriteLine("Введите число b:");
+b = Convert.ToInt32(Console.ReadLine());
+ 
+for (int i = a; i <= b; i++) {
+    p *= i;
+}
+ 
+Console.WriteLine("Произведение: " + p);
+Console.Read();
