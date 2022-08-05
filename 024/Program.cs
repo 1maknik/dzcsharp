957bf4b7c833815766d5bf0e03ccb091c3e60623
nik <1maknik@mail.ru>
2022-08-05 16:58:22 +0300

    z024

---
+++ 024/Program.cs
@@ -0,0 +1,9 @@
+﻿
+            Console.Write("Введите кол-во чисел для вывода:");
+            int N = int.Parse(Console.ReadLine());
+ 
+            Console.WriteLine("Число" + "\t" + "Квадрат");
+            for(int i = 1; i <= N; i++)
+                Console.WriteLine(i + "\t" + i*i);
+ 
+            Console.ReadLine();
