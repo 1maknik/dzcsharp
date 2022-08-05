4a197f6f716766b63f61420979b5257e22af8e8f
nik <1maknik@mail.ru>
2022-08-05 17:32:33 +0300

    z026

---
+++ 026/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите а: ");
+            int a = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Введите b: ");
+            int b = Convert.ToInt16(Console.ReadLine());
+ 
+            int sum = 0;
+ 
+            if (a > b)
+            {
+                Console.WriteLine("Число а больше числа b");                
+            }
+            for (int i = a; i<= b; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("Сумма чисел от " + a + " до " + b + ": " + sum);
