77fcf351ed8aa8c7a1ea53e5ac2f90d2abd7c777
nik <1maknik@mail.ru>
2022-08-05 17:18:24 +0300

    z025

---
+++ 025/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+		int firstnumb = int.Parse(Console.ReadLine());
+		int firstkyb = 1;
+		int i = 0;
+		
+		for(i=0; i<3; i++)
+		{
+			firstkyb *= firstnumb;
+		}
+		
+		Console.WriteLine("Куб числа: " + firstkyb );
